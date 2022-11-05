f48ed70339f084593a656e90c4726a58d69e9349
Espe Cabrer <mecabrerv@gmail.com>
2022-11-05 14:02:21 +0100

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,47 @@
+<script>
+    import { onMounted, ref } from 'vue';
+export default {
+    setup() {
+        const name = ref('')
+        const el = ref()
+
+        const submitForm = () => {
+            console.log(`Form submitted! Name = ${name.value}`)
+        }
+
+        onMounted(()=> {
+            el.value.focus()
+        })
+
+        return {
+            el,
+            name,
+            submitForm
+        }
+    },
+};
+</script>
+
+<template>
+    <div>
+        <p class="text-3xl font-bold underline bg-rose-500">{{name}}</p>
+        <input ref="el" type="text" placeholder="Name" v-model="name"/>
+        <div>
+            <button 
+            @click="submitForm"
+            v-bind:disabled="name.length == 0"
+            >Submit</button>
+        </div>
+    </div>
+</template>
+
+<style>
+input[type='text'] {
+    padding: 5px;
+}
+
+button { 
+    margin-top: 20px;
+}
+
+</style>
